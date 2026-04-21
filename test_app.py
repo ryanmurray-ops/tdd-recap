879af58f319103f06f7c76b99cf8487eea74099b
Ryan Murray <ryan.murray@madetech.com>
2026-04-21 11:18:03 +0100

    add test to check length of returned duties is greater than 0

--- test_app.py
+++ test_app.py
@@ -1,5 +1,9 @@
 from app import list_duties
 
 def test_list_duties_returns_a_list_component():
-    list_duties()
-    assert list_duties() == []
+    result = list_duties()
+    assert isinstance(result, list)
+
+def test_list_duties_returns_at_least_one_duty():
+    result = list_duties()
+    assert len(result) > 0
