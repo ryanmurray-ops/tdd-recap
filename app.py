708cdcdb2cf279fa5fb0a634732d554f46ae4bfe
Ryan Murray <ryan.murray@madetech.com>
2026-04-21 11:39:41 +0100

    add test to loop through duties and confirm all 13 duties exist

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def list_duties():
-    return ['duty1', 'duty2']
+    return ['duty1', 'duty2', 'duty3', 'duty4', 'duty5', 'duty6', 'duty7', 'duty8', 'duty9', 'duty10', 'duty11', 'duty12', 'duty13']
--- test_app.py
+++ test_app.py
@@ -8,4 +8,15 @@ def test_list_duties_returns_at_least_one_duty():
     result = list_duties()
     assert len(result) > 0
     assert 'duty1' in result
+
+def test_list_duties_returns_at_least_two_duties():
+    result = list_duties()
+    assert len(result) > 1
     assert 'duty2' in result
+
+def test_list_duties_returns_all_duties():
+    duty_list = ['duty1', 'duty2', 'duty3', 'duty4', 'duty5', 'duty6', 'duty7', 'duty8', 'duty9', 'duty10', 'duty11', 'duty12', 'duty13']
+    result = list_duties() 
+
+    for duty in duty_list:
+        assert duty in result
