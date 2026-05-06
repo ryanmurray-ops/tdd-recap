570c56b2f7b4438eaf707faa2f67715eff1622df
Ryan Murray <ryan.murray@madetech.com>
2026-05-06 12:31:41 +0100

    test: homepage displays ass duties relating to the automate coin

--- tests/integration/test_home_page.py
+++ tests/integration/test_home_page.py
@@ -29,6 +29,12 @@ def test_home_page_formats_duty_5_correctly():
     response = test_app.get('/')
     assert "Duty 5 - Build and operate a continuous Integration (CI) capability, employing version control of source code and related artifacts" in response.text
 
+def test_homepage_shows_all_automate_duties():
+    response = test_app.get('/')
+    assert "Duty 5" in response.text
+    assert "Duty 7" in response.text
+    assert "Duty 10" in response.text
+
 # def test_home_page_is_reachable(page:Page):
 #     page.goto("localhost:5000/")
 #     expect(page).to_have_title(re.compile("Index"))
