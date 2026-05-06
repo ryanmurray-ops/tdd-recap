1c074efe7597fc70fcf909b6c9826d33a03f1824
Ryan Murray <ryan.murray@madetech.com>
2026-05-06 10:59:23 +0100

    test: homepage displays duty 5

--- tests/integration/test_home_page.py
+++ tests/integration/test_home_page.py
@@ -17,6 +17,10 @@ def test_automate_is_homepage_heading():
     response = test_app.get('/')
     assert "<h1>Automate</h1>" in response.text
 
+def test_home_page_shows_duty_5():
+    response = test_app.get("/")
+    assert "Duty 5" in response.text
+
 # def test_home_page_is_reachable(page:Page):
 #     page.goto("localhost:5000/")
 #     expect(page).to_have_title(re.compile("Index"))
