0e7e7c6184e7fcaa8038d83ec021754568c2a224
Ryan Murray <ryan.murray@madetech.com>
2026-05-06 11:14:34 +0100

    feat: display duty 5 description on homepage

--- db.py
+++ db.py
@@ -1,5 +1,8 @@
 def get_all_duties():
     return [
-        {"identifier": 5, "description": "Build CI pipeline"}
+        {
+            "identifier": 5, 
+            "description": "Build and operate a continuous Integration (CI) capability, employing version control of source code and related artifacts"
+        }
     ]
 
