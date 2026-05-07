e348441d7ea686cc01ee7ed682010c254385ac10
Ryan Murray <ryan.murray@madetech.com>
2026-05-07 10:56:57 +0100

    update add_duty_route to call add_duty function and render stored duties

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 from flask import Flask, render_template, request
 
-from db import get_all_duties
+from db import add_duty, get_all_duties
 
 app = Flask(__name__)
 
 @app.route("/")
 def list_duties():
-    database_duties = get_all_duties()
-    return render_template("index.html", duties= database_duties)
+    duties = get_all_duties()
+    return render_template("index.html", duties = duties)
 
 @app.route("/add-duty", methods=["POST"])
-def add_duty():
+def add_duty_route():
     new_duty = {
         "identifier": request.form["identifier"],
         "description": request.form["description"]
 
     }
     
-    return new_duty
+    add_duty(new_duty)
+
+    duties = get_all_duties()
+
+    return render_template("index.html", duties = duties)
 
 if __name__ == "__main__":
     app.run(debug=True)
