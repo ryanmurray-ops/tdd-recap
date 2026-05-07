e207c7a2e45ff79122239a0cf33d7d9dadef02c3
Ryan Murray <ryan.murray@madetech.com>
2026-05-07 16:35:16 +0100

    test: homepage has add duty form

--- tests/integration/test_home_page.py
+++ tests/integration/test_home_page.py
@@ -98,6 +98,11 @@ def test_duplicate_duty_identifier_is_not_added():
 
     assert response.text.count("Duty 12") == 1
 
+def test_homepage_has_add_duty_form():
+    response = test_app.get('/')
+
+    assert "<form>" in response.text
+
 # def test_home_page_is_reachable(page:Page):
 #     page.goto("localhost:5000/")
 #     expect(page).to_have_title(re.compile("Index"))
