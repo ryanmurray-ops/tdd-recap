eb98508ef34f217c04fb452447eb77b0b0ac48df
Ryan Murray <ryan.murray@madetech.com>
2026-05-07 10:46:37 +0100

    create variable outside of get_all_duties function to store duties

--- db.py
+++ db.py
@@ -1,16 +1,18 @@
+duties = [
+    {
+        "identifier": 5, 
+        "description": "Build and operate a continuous Integration (CI) capability, employing version control of source code and related artifacts"
+    },
+    {
+        "identifier": 7, 
+        "description": "Provision cloud infrastructure using APIs, continually improve infrastructure-as-code, considering use of industry leading technologies as they become available (e.g. Serverless, Containers)"
+    },
+    {
+        "identifier": 10, 
+        "description": "Implement a good coverage of monitoring (metrics, logs), ensuring that alerts are visible, tuneable and actionable"
+    }
+]
+
 def get_all_duties():
-    return [
-        {
-            "identifier": 5, 
-            "description": "Build and operate a continuous Integration (CI) capability, employing version control of source code and related artifacts"
-        },
-        {
-            "identifier": 7, 
-            "description": "Provision cloud infrastructure using APIs, continually improve infrastructure-as-code, considering use of industry leading technologies as they become available (e.g. Serverless, Containers)"
-        },
-        {
-            "identifier": 10, 
-            "description": "Implement a good coverage of monitoring (metrics, logs), ensuring that alerts are visible, tuneable and actionable"
-        }
-    ]
+    return duties
 
