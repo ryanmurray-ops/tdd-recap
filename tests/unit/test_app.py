22a05651f3eac799cb930f7057ca09c92b031243
Ryan Murray <ryan.murray@madetech.com>
2026-05-09 14:09:17 +0100

    test: isolate unit tests using pytest fixture to reset state

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -2,6 +2,25 @@ import pytest
 from app import list_duties
 import db
 
+@pytest.fixture(autouse=True)
+def reset_duties():
+    db.duties.clear()
+    db.duties.extend([
+        {
+        "identifier": 5, 
+        "description": "Build and operate a continuous Integration (CI) capability, employing version control of source code and related artifacts"
+    },
+    {
+        "identifier": 7, 
+        "description": "Provision cloud infrastructure using APIs, continually improve infrastructure-as-code, considering use of industry leading technologies as they become available (e.g. Serverless, Containers)"
+    },
+    {
+        "identifier": 10, 
+        "description": "Implement a good coverage of monitoring (metrics, logs), ensuring that alerts are visible, tuneable and actionable"
+    }
+    ])
+
+    db.error_message = None
 # def test_list_duties_returns_a_list_component():
 #     result = list_duties()
 #     assert isinstance(result, list)
