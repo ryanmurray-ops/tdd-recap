8226ff647c0826a2b9df15c88ca3cddb364436d8
Ryan Murray <ryan.murray@madetech.com>
2026-05-09 14:23:11 +0100

    test: add failing test for duty description label for form input

--- tests/integration/test_home_page.py
+++ tests/integration/test_home_page.py
@@ -117,6 +117,10 @@ def test_homepage_has_identifier_label():
     response = test_app.get("/")
     assert '<label for="identifier">Duty Number</label>' in response.text
 
+def test_homepage_has_identifier_label():
+    response = test_app.get("/")
+    assert '<label for="identifier">Duty description</label>' in response.text
+
 def test_homepage_form_has_submit_button():
     response = test_app.get("/")
 
