cc57d7fcd8d6b9a7c0fd2793a83ce7accb80fd4b
Ryan Murray <ryan.murray@madetech.com>
2026-05-10 14:36:13 +0100

    test: add_duty stores duty as objects

--- tests/unit/test_duty.py
+++ tests/unit/test_duty.py
@@ -1,3 +1,4 @@
+import db
 from duty import Duty
 
 def test_duty_class_stores_identifier():
@@ -7,3 +8,13 @@ def test_duty_class_stores_identifier():
 def test_duty_class_stores_description():
     new_duty = Duty("12", "Test description")
     assert new_duty.description == "Test description"
+
+def test_add_duty_stores_a_duty_object():
+    new_duty = {
+        "identifier": "15",
+        "description": "Object test"
+    }
+
+    db.add_duty(new_duty)
+    duties = db.get_all_duties()
+    assert isinstance(duties[-1], Duty)
